3cc2ff043b387fb3b0edbf3790222ccb0dccf0d1
Isaac Lopes de Medeiros <62267177+isaacmeedeiros@users.noreply.github.com>
2021-10-27 18:37:51 +0000

    Questão inverter

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <array>
-
+#include <algorithm>    // std::swap
 /*! 
  * Reverse de order of elements inside the array.
  * @param arr Reference to the array with the values.
@@ -8,5 +8,18 @@
 template <std::size_t SIZE>
 void reverse( std::array< std::string, SIZE > & arr )
 {
-    /*codigo*/
+
+if((SIZE % 2) == 0){ //SIZE PAR
+  for(int x = 0; x<(SIZE/2);x++){ 
+    std::swap (arr[x],arr[SIZE-x-1]);
+  
+  }
 }
+else if((SIZE % 2)!= 0){
+  int aux = SIZE;
+  for(int x = 0; x<((SIZE-1)/2); x++){
+    std::swap (arr[x],arr[SIZE-x-1]);
+  }
+}
+}
+
